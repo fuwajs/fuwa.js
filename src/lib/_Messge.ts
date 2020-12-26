cdd3655d01b95dc53e89abdca39ff3bd55d782d3
Artrix9095 <39530102+Artrix9095@users.noreply.github.com>
2020-12-25 16:05:39 -0800

    Update _Messge.ts

--- src/lib/_Messge.ts
+++ src/lib/_Messge.ts
@@ -1,3 +1,20 @@
+import { Embed } from "./Embed";
+
+export interface file  { 
+content :  string ; 
+}
+
+export interface allowedMention { 
+mentions : "users" | "roles" | "everyone"; 
+check? : boolean;
+}
+
 export class Message { 
-    
+content :  string ;
+private nonce : number | string ;
+tts  :  boolean; 
+file : file; 
+embed :Embed; 
+allowedMentions: 
+
 }
