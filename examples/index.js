9a12759bfa59abe77743c05c0998904ee87d621a
Phoenix <PhoenixWT@protonmail.com>
2021-01-05 11:05:07 -0500

    Format with prettier

--- examples/index.js
+++ examples/index.js
@@ -1,26 +1,26 @@
 const { fs, path } = { fs: require('fs'), path: require('path') }; // Optional if you use login option 2
-const Fuwa = require('../dist/index'); // Import fuwa.js here!
+const fuwa = require('../dist/index'); // Import fuwa.js here!
 
-const client = new Fuwa.Client(req => '?',  { debug: true });
+const client = new fuwa.Client(req => '?', { debug: true });
 
 client
-// This function is going to be ran when the bot finishes loading and connects to discord
+    // This function is going to be ran when the bot finishes loading and connects to discord
     .on('READY', () => console.log('POG'))
     .use((_, __, next) => {
         console.log('I WAS USED');
         next();
     })
     .command('ping', (req, res, next) => {
-        res.send(new Fuwa.Embed()
+        res.send(new fuwa.Embed()
             .setTitle('Pong')
-            .setColor(Fuwa.Colors.rgb(13, 186, 120))
+            .setColor(fuwa.Colors.rgb(13, 186, 120))
         );
     })
     // Set your status!
     .setStatus({
-        name: 'Some Status', 
+        name: 'Some Status',
         type: 'streaming',
-        url: 'https://blank.org' 
+        url: 'https://blank.org'
     });
 
 
--- src/lib/_Role.ts
+++ src/lib/_Role.ts
@@ -1,39 +1,39 @@
-export interface _Role { 
-    name: string; 
-    permissions?: string; 
+export interface _Role {
+    name: string;
+    permissions?: string;
     color?: number;
-    hoist?: boolean; 
+    hoist?: boolean;
     mentionable?: boolean;
-};
+}
 
-class Role { 
-    name: string; 
-    permissions: string; 
-    color: number; 
-    hoist: boolean = false; 
+class Role {
+    name: string;
+    permissions: string;
+    color: number;
+    hoist: boolean = false;
     mentionable: boolean = false;
-    constructor ( data: _Role ) {
-        this.name = data.name; 
+    constructor(data: _Role) {
+        this.name = data.name;
         if (data.permissions) {
             this.permissions = data.permissions;
         } else {
             this.permissions = ' ';
         }
-        if (data.color) { 
+        if (data.color) {
             this.color = data.color;
         } else {
             this.color = 0;
         }
-        if (data.hoist) { 
-            this.hoist = true
+        if (data.hoist) {
+            this.hoist = true;
         }
-        if (data.mentionable) { 
+        if (data.mentionable) {
             this.mentionable = true;
         }
     }
-    get roles() { 
+    get roles() {
         return this;
     }
-};
+}
 
 export default Role;
