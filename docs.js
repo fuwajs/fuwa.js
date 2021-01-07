d9e9b81c958a28244600deabfe3e064196433d07
Artrix <39530102+Artrix9095@users.noreply.github.com>
2021-01-07 13:37:17 -0800

    add docs to gitignore

--- docs.js
+++ docs.js
@@ -2,9 +2,9 @@ const fs = require('fs');
 const path = require('path'); 
 
 module.exports = {
-    "entryPoints": [...fs.readdirSync(path.join(__dirname, 'src', 'lib'))
+    entryPoints: [...fs.readdirSync(path.join(__dirname, 'src', 'lib'))
         .map(v => !v.startsWith('_') ? path.join(__dirname, 'src', 'lib', v) : null), 
         path.join(__dirname, 'src', 'index.ts')],
-    "out": path.join(__dirname, '..', 'docs'),
-    "readme": "README.md"
+    out: path.join(__dirname, '..', 'docs'),
+    readme: 'README.md'
 }
