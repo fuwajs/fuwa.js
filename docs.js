852bab49270f66ac98fbb7cfcc3ddaf4652f4465
Artrix9095 <sefu786@outlook.com>
2021-01-07 12:47:14 -0800

    add docs.js

---
+++ docs.js
@@ -0,0 +1,10 @@
+const fs = require('fs');
+const path = require('path'); 
+
+module.exports = {
+    "entryPoints": [...fs.readdirSync(path.join(__dirname, 'src', 'lib'))
+        .map(v => !v.startsWith('_') ? path.join(__dirname, 'src', 'lib', v) : null), 
+        path.join(__dirname, 'src', 'index.ts')],
+    "out": "docs",
+    "readme": "README.md"
+}
