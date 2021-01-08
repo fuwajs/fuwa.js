3b895e88a351907c06ae710bc45b1d3a975af353
Phoenix <PhoenixWT@protonmail.com>
2021-01-08 09:34:31 -0500

    docs & formatting

--- src/lib/_Role.ts
+++ src/lib/_Role.ts
@@ -10,19 +10,19 @@ class Role {
     hoist: boolean = false;
     mentionable: boolean = false;
     managed: boolean = false;
-    position: number
-    id: string; 
+    position: number;
+    id: string;
     permissionsNew: string;
     constructor(data: _Role) {
         this.name = data.name;
         this.id = data.id;
         this.position = data.position;
-        if (data.permissions_new) { 
+        if (data.permissions_new) {
             this.permissionsNew = data.permissions_new;
         }
         if (data.permissions) {
             this.permissions = data.permissions;
-        } 
+        }
         if (data.color) {
             this.color = data.color;
         } else {
