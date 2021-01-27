da96cdf144c043ea88b44b39e4944ff99f9d0ad6
Artrix9095 <sefu786@outlook.com>
2021-01-26 16:36:26 -0800

    test deno & browser support

--- src/index.ts
+++ src/index.ts
@@ -3,4 +3,4 @@ export {default as Embed} from './lib/discord/Embed';
 export {default as Colors} from './lib/Colors';
 // for canadians
 export {default as Colours} from './lib/Colors';
-export { Argument } from './lib/Command';
+// export { Argument } from './lib/Command';
