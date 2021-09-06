5f4351b8739fea304da2f80ddd3c45e2dd25a740
Phoenix <PhoenixWT@protonmail.com>
2021-09-06 18:37:51 -0400

    Housekeeping: Remove /dist
    Previously, this package could only be downloaded via GitHub, hence the
    dist directory was kept in version-control. Now we have an npm package,
    so this isn't necessary anymore.

--- dist/lib/_globals.d.ts
+++
@@ -1,5 +0,0 @@
-import User from './discord/User';
-export declare let token: string | null;
-export declare let bot: User | null;
-export declare function setToken(newToken: string): string;
-export declare function setBot(user: User): User;
