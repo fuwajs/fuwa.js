d20902fcbd32c68ef926028c3aa3d4f2d6cafc57
Artrix <39530102+Artrix9095@users.noreply.github.com>
2021-09-07 20:55:26 -0700

    add slash commands type

--- src/lib/_DiscordAPI.ts
+++ src/lib/_DiscordAPI.ts
@@ -424,6 +424,19 @@ export interface Ready {
     application: Application;
 }
 
+export enum SlashCommandTypes {
+    subCommand = 1,
+    subCommandGroup = 2,
+    string = 3,
+    integer = 4,
+    boolean = 5,
+    user = 6,
+    channel = 7,
+    role = 8,
+    mentionable = 9,
+    number = 10,
+}
+
 export interface Application {
     id: string;
     flags: number;
