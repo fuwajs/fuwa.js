e23e0568148f8b13566b3754f174a6b9fc365af2
ThatGuyJamal <jamallyons8@gmail.com>
2021-09-28 20:14:32 -0400

    Create http.ts

---
+++ src/lib/structures/ws/http.ts
@@ -0,0 +1,13 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+/* eslint-disable @typescript-eslint/ban-ts-comment */
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable no-async-promise-executor */ // should be fixed soon
+/******************************************************************************
+ * @file src/lib/_http.ts
+ * @fileoverview Provides http functions using undici or fetch() depending on
+ * the JavaScript/TypeScript environment.
+ *****************************************************************************/
+
+/**
+ * TODO http for ApplicationCommands
+ */
