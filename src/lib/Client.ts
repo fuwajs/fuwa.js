be4519b23855931ff9c27c6a1a6e90bfed652f9f
ThatGuyJamal <jamallyons8@gmail.com>
2021-09-28 20:14:28 -0400

    Update Client.ts
    adding more client options

--- src/lib/Client.ts
+++ src/lib/Client.ts
@@ -1,11 +1,29 @@
 import { WebSocket } from '../lib/structures/index';
-import { discordAPI, GatewayCodes, GatewayIntents } from '../interfaces/DiscordAPI';
+import { discordAPI, DiscordAPIOP, GatewayCodes, GatewayIntents } from '../interfaces/DiscordAPI';
 import Command from './structures/handlers/Command';
 import Globs from '../util/Global';
+import {debug as Debug} from "../util/Debug"
 
 export interface ClientOptions {
+    /**Discord Bot Token */
     token?: string | Buffer;
+    /**
+     * @description Discord Intends, enabling bot functions with our api.
+     * @see https://discord.com/developers/docs/topics/gateway#gateway-intents
+     */
     intents?: (keyof typeof GatewayIntents)[];
+    /**
+     * The owner(s) discord ID. These users can bypass default bot permissions.
+     */
+    owners?: string[] | string;
+    /**
+     * TODO shard manager
+     */
+    shards?: number;
+    /**
+     *
+     */
+    applicationId?: string;
 }
 export interface Events {
     ready(shardId?: number): any;
@@ -18,18 +36,42 @@ export default class Client extends WebSocket {
     public defaultPrefix: string | null;
     protected intents: (keyof typeof GatewayIntents)[];
     protected readonly token = '';
-    public readonly uptime;
+    public shardCount: number;
+    public applicationId: string;
+    protected options;
+    protected debug: typeof Debug;
     public constructor(options?: ClientOptions) {
         super();
         Object.assign(this, {
             intents: ['DirectMessages', 'Guilds', 'GuildMessages'],
             ...options,
         });
+        if (options) {
+            this.applicationId = options.applicationId;
+            this.shardCount = options.shards ?? 0;
+        }
+        this.shardCount = 0;
+        this.options = {
+            intents:
+                GatewayIntents.Guilds |
+                GatewayIntents.GuildMessages |
+                GatewayIntents.GuildBans |
+                GatewayIntents.DirectMessages,
+            ...options,
+        };
     }
 
+    /**
+     * @typeParam T The event name
+     * @param cb The callback function
+     * ```typescript
+     * <clint>.on('ready', () => console.log ('Up and ready to go!'));
+     * ```
+     */
     public on<T extends keyof Events>(event: T, callback: Events[T]) {
         this.events.set(event, callback);
     }
+
     public login(token?: string | Buffer) {
         const _token = this.token || token.toString();
         Globs.token = _token;
@@ -59,6 +101,11 @@ export default class Client extends WebSocket {
             this.events.has('ready') ? this.events.get('ready')(ready.shard) : void 0;
         });
     }
+
+    /**
+     * TODO remove to commands client plugin when its created.
+     * this client will be the core and only deal with interactions (latest discord api)
+     */
     public async fetchPrefix(): Promise<string> {
         if (this.defaultPrefix === null) {
             throw new Error(
@@ -66,5 +113,18 @@ export default class Client extends WebSocket {
             );
         }
         return this.defaultPrefix;
+    }
+
+    /**
+     * 
+     * @param shardId the shard(s) spawned from websocket
+     * @param data discord raw api json
+     */
+    protected async spawnShard(shardId: number, data: DiscordAPIOP[GatewayCodes.Identify]['d']) {
+        this.response.op.emit(GatewayCodes.Identify, {
+            ...data,
+            shard: [shardId, this.shardCount],
+        });
+        this.debug.log('shards', `Sent shard ${shardId}`);
     }
 }
