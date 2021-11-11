a6b9fde22acc003519207068f2fce175b66613a0
Phoenix <PhoenixWT@protonmail.com>
2021-11-11 17:26:04 -0500

    Housekeeping: Ignore the deno folder
    It's a generated folder

--- deno/index.ts
+++
@@ -1,5 +0,0 @@
-export * from './lib/structures/index.ts';
-
-export { Embed, EmbedType, Media as EmbedMedia } from './lib/discord/Embed.ts';
-
-export { default as Attachment } from './lib/discord/Attachment.ts';
--- deno/lib/structures/index.ts
+++
@@ -1,4 +0,0 @@
-// handlers
-export { Command, Argument } from './handlers/Command.ts';
-export { Client, ClientOptions } from './handlers/Client.ts';
-export { PluginOptions, Plugin } from './handlers/Plugin.ts';
