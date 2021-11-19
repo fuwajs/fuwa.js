881e4baee791aa3a7ee115d71fa3379f9f5d1acc
Artrix <39530102+Artrix9095@users.noreply.github.com>
2021-11-19 12:54:00 -0800

    add more, and more simplified export

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,2 @@
 export * from './lib/structures';
-
-export { Embed, EmbedType, Media as EmbedMedia } from './lib/discord/Embed';
-
-export { default as Attachment } from './lib/discord/Attachment';
+export * from './lib/discord';
---
+++ src/lib/discord/index.ts
@@ -0,0 +1,9 @@
+export { default as Attachment } from './Attachment';
+export { default as Context } from './Context';
+
+export * from './Button';
+export * from './Guild';
+export * from './User';
+export * from './Message';
+export * from './Embed';
+export * from './Channel';
