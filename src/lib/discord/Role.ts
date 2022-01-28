91e4b204c8a81757c969e6ea4ac1b09ab08a0de6
Artrix <39530102+Artrix9095@users.noreply.github.com>
2022-01-28 08:45:18 -0800

    add some new stuff to role

--- src/lib/discord/Role.ts
+++ src/lib/discord/Role.ts
@@ -1,17 +1,18 @@
-import { Role as RoleData } from '../../interfaces/guild'
+import { DISCORD_API, PermissionFlags } from '../../interfaces';
+import { Role as RoleData } from '../../interfaces/guild';
+import { enumEqualityTester, formatImageURL } from '../../util';
 
 export class Role {
-    constructor(protected data: RoleData) {
-    }
-    
+    constructor(protected data: RoleData) {}
+
     public get id() {
         return this.data.id;
     }
     public get name() {
         return this.data.name;
     }
-    public get color() {
-        return this.data.color;
+    public get color(): number | null {
+        return this.data.color !== 0 ? this.data.color : null;
     }
     public get hoist() {
         return this.data.hoist;
@@ -19,13 +20,22 @@ export class Role {
     public get mentionable() {
         return this.data.mentionable;
     }
+    public get icon() {
+        return this.data.icon ? `${DISCORD_API}/${formatImageURL(this.data.icon, 512)}` : null;
+    }
     public get position() {
         return this.data.position;
     }
     public get permissions() {
-        return this.data.permissions;
+        const perms = parseInt(this.data.permissions);
+        const payload: { [key in keyof typeof PermissionFlags]: boolean } = {} as any;
+        Object.keys(PermissionFlags).forEach(key => {
+            const val = PermissionFlags[key];
+            payload[key] = (perms & val) === val;
+        });
+        return payload;
     }
-    public get managed() {
+    public get isManaged() {
         return this.data.managed;
     }
 }
